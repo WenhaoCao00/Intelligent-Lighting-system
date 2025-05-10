829638bfba364a10c9a565875528839e30ff772a
WenhaoCao <101616974+WenhaoCao00@users.noreply.github.com>
2025-05-10 21:49:59 +0200

    Create circleSwitch.py
    Create circleSwitch.py

---
+++ raspberry/plugwise/circleSwitch.py
@@ -0,0 +1,24 @@
+from plugwise.api import *
+from time import sleep
+from datetime import datetime
+DEFAULT_PORT = "/dev/ttyUSB0"
+mac = "000D6F0004B1E6C4"  # Circle MAC
+
+stick = Stick(DEFAULT_PORT)
+circle = Circle(mac, stick)
+
+current_time = datetime.now()
+circle.set_clock(current_time)
+
+sleep(1)
+
+
+state = circle.get_info()
+print(state)
+# toggle switch
+if state["relay_state"] == 1:
+    print("Now turning off...")
+    circle.switch_off()
+else:
+    print("Now turning on...")
+    circle.switch_on()
